435343ee14e8448db0cbaddb398314b9aa2af7ae
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-12-08 13:09:43 +0500

    Update 1_Home_Page.py

--- 1_Home_Page.py
+++ 1_Home_Page.py
@@ -24,8 +24,17 @@ def main():
     col1, col2 = st.columns([1, 1])  # Adjust the ratio if needed
     with col1:
         st.markdown("<h1 style='font-size: 60px; margin-top: 20px;'>AI DRIVEN DAILY NEWS CONTENT GENERATOR</h1>", unsafe_allow_html=True)
-        st.markdown("<p style='font-size: 20px; color: gray;'>Empowering you to stay ahead with AI-curated, real-time news from around the globe. Experience the future of content generation, tailored just for you! 📰🌍📡 </p>",
+        st.markdown("<h1 style='font-size: 80px;'>AI DRIVEN DAILY NEWS CONTENT GENERATOR</h1>", unsafe_allow_html=True)
+        st.markdown("<p style='font-size: 20px; color: gray;'>Empowering you to stay ahead with AI-curated, real-time news from around the globe. 📰🌍📡 Experience the future of content generation, tailored just for you!</p>",
         unsafe_allow_html=True)
+        st.markdown("<p style='font-size: 20px; color: gray;'>Your trusted partner for reliable, dynamic, and innovative news solutions. 🚀🤝</p>",
+        unsafe_allow_html=True)
+        st.markdown("""<ul style='font-size: 18px; color: gray;'>
+               <li>🌟 Generate news articles in seconds.</li>
+               <li>🧠 Powered by cutting-edge AI algorithms.</li>
+               <li>🌍 Covering global events with precision.</li>
+               <li>📡 Designed for journalists, students, and professionals alike.</li>
+           </ul>""",unsafe_allow_html=True)
     with col2:
         st.image("logo.png", width=600)
 
